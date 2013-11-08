12da832dbc2b0cd83e84d4faf92dbdf2d8909886
bkargoll <bastian.kargoll@cern.ch>
2013-11-08 17:43:21 +0100

    add config to use PAT jets

--- CRAB/TauNtuple_PatJets_cfg.py
+++ CRAB/TauNtuple_PatJets_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 import os
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 
-process = cms.Process("AOD")
+process = cms.Process("TauNTuple")
 
 process.load('Configuration/StandardSequences/Services_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
@@ -20,6 +20,52 @@ process.load("Configuration.StandardSequences.EndOfProcess_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+######################################################
+#
+# load Pat
+#
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+from PhysicsTools.PatAlgos.tools.metTools import *
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+removeMCMatching(process, ['All'])
+
+# Jet energy corrections to use:
+if "<DataType>" == "Data":
+    inputJetCorrLabel = ('AK5PF', ['L1Offset', 'L2Relative', 'L3Absolute', 'L2L3Residual'])
+else:
+    inputJetCorrLabel = ('AK5PF', ['L1Offset', 'L2Relative', 'L3Absolute'])
+process.patJetCorrFactors.useRho=False
+
+# add pf met
+addPfMET(process, 'PF')
+
+# Add PF jets
+switchJetCollection(process,cms.InputTag('ak5PFJets'),
+                    doJTA        = True,
+                    doBTagging   = True,
+                    jetCorrLabel = inputJetCorrLabel,
+                    doType1MET   = True,
+                    genJetCollection=cms.InputTag("ak5GenJets"),
+                    doJetID      = True
+                    )
+process.patJets.addTagInfos = True
+process.patJets.tagInfoSources  = cms.VInputTag(
+    cms.InputTag("secondaryVertexTagInfosAOD"),
+    )
+
+
+# load the PU JetID sequence
+process.load("CMGTools.External.pujetidsequence_cff")
+
+# Select jets
+process.selectedPatJets.cut = cms.string('pt > 18')
+
+#from PhysicsTools.PatUtils.metUncertaintyTools import runMEtUncertainties
+#runMEtUncertainties(process)
+
+####### done with setting up PAT
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
@@ -49,28 +95,6 @@ process.CountTriggerPassedEvents = process.EvntCounterB.clone()
 process.CountTriggerPassedEvents.CounterType = cms.untracked.string("CountTriggerPassedEvents")
 process.CountKinFitPassedEvents = process.EvntCounterB.clone()
 process.CountKinFitPassedEvents.CounterType = cms.untracked.string("CountKinFitPassedEvents")
-
-####################### Jet corrections ######################
-process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
-process.load("JetMETCorrections.Configuration.DefaultJEC_cff")
-
-# this should load the correction service and the corresponding producers
-# one producer is e.g. ak5PFJetsL1FastL2L3 (recommendated for MC) and ak5PFJetsL1FastL2L3Residual  (recommendated for data)
-# run one of these to create a new branch which contains the corrected jets
-
-if "<DataType>" == "Data":
-    jetCorrectionProducer = process.ak5PFJetsL1FastL2L3Residual
-    process.NtupleMaker.pfjets = cms.InputTag('ak5PFJetsL1FastL2L3Residual')
-else:
-    jetCorrectionProducer = process.ak5PFJetsL1FastL2L3
-    process.NtupleMaker.pfjets = cms.InputTag('ak5PFJetsL1FastL2L3')
-    
-####################### Jet Flavour for bTagging ######################  
-if "<DataType>" == "Data":
-    jetFlavourSequence = cms.Sequence()
-else:
-    process.load("PhysicsTools.PatAlgos.mcMatchLayer0.jetFlavourId_cff")
-    jetFlavourSequence = process.patJetFlavourId
                         
 ####################### MET corrections ######################
 process.load("JetMETCorrections.Type1MET.pfMETCorrections_cff")
@@ -107,6 +131,12 @@ process.NtupleMaker.EleMVAWeights5 = cms.untracked.string(base+'/data/Electrons_
 process.NtupleMaker.EleMVAWeights6 = cms.untracked.string(base+'/data/Electrons_BDTG_TrigNoIPV0_2012_Cat6.weights.xml')
 process.NtupleMaker.ElectronMVAPtCut = cms.double(28.0);
 
+# set Ntuple to run on PAT jets
+process.NtupleMaker.doPatJets = cms.untracked.bool(True)
+process.NtupleMaker.srcPatJets = cms.untracked.string("selectedPatJets")
+process.NtupleMaker.doPatMET = cms.untracked.bool(True)
+process.NtupleMaker.srcPatMET = cms.untracked.string("patMETsPF")
+
 process.schedule = cms.Schedule()
 
 process.NtupleMaker.doTrack= cms.untracked.bool(False)
@@ -114,7 +144,7 @@ process.NtupleMaker.doTrack= cms.untracked.bool(False)
 process.MultiTrigFilter.useTriggers = cms.vstring("HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v","HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v","HLT_IsoMu24_eta2p1_v","HLT_Mu17_Ele8_CaloIdL","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL","HLT_Mu8_Ele17_CaloIdL",
 "HLT_Mu8_Ele17_CaloIdT_CaloIsoVL","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL")
 
-process.TauNutpleSkim  = cms.Path(process.EvntCounterA*process.MultiTrigFilter*process.MuonPreselectionCuts*process.CountTriggerPassedEvents*process.PFTau*process.PreselectionCuts*jetCorrectionProducer*process.type0PFMEtCorrection*process.producePFMETCorrections*jetFlavourSequence*process.EvntCounterB*process.NtupleMaker)
+process.TauNutpleSkim  = cms.Path(process.EvntCounterA*process.MultiTrigFilter*process.MuonPreselectionCuts*process.CountTriggerPassedEvents*process.patDefaultSequence*process.puJetIdSqeuence*process.PFTau*process.PreselectionCuts*process.EvntCounterB*process.NtupleMaker)
 process.schedule.append(process.TauNutpleSkim)
 
 
