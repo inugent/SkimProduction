40ae2499427bc8309d67c17df56ee4fdddd91812
bkargoll <bastian.kargoll@cern.ch>
2013-11-20 17:03:33 +0100

    version with not working MET corrections

--- CRAB/TauNtuple_PatJets_cfg.py
+++ CRAB/TauNtuple_PatJets_cfg.py
@@ -9,14 +9,13 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
-<globaltag>
-#process.GlobalTag.globaltag = 'START52_V9::All'
+#<globaltag>
+process.GlobalTag.globaltag = 'START52_V9::All'
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.load("Configuration.EventContent.EventContent_cff")
 process.load('HLTrigger.Configuration.HLT_GRun_cff')
 
-process.load("Configuration.StandardSequences.EndOfProcess_cff")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
@@ -28,6 +27,7 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from PhysicsTools.PatAlgos.tools.coreTools import *
 from PhysicsTools.PatAlgos.tools.metTools import *
 from PhysicsTools.PatAlgos.tools.jetTools import *
+from PhysicsTools.PatAlgos.tools.pfTools import *
 
 removeMCMatching(process, ['All'])
 
@@ -37,9 +37,6 @@ if "<DataType>" == "Data":
 else:
     inputJetCorrLabel = ('AK5PF', ['L1Offset', 'L2Relative', 'L3Absolute'])
 process.patJetCorrFactors.useRho=False
-
-# add pf met
-addPfMET(process, 'PF')
 
 # Add PF jets
 switchJetCollection(process,cms.InputTag('ak5PFJets'),
@@ -57,20 +54,68 @@ process.patJets.tagInfoSources  = cms.VInputTag(
 
 
 # load the PU JetID sequence
-process.load("CMGTools.External.pujetidsequence_cff")
+#process.load("CMGTools.External.pujetidsequence_cff") # should not be needed, is done below with MVA MET
 
 # Select jets
 process.selectedPatJets.cut = cms.string('pt > 18')
+
+############ MET #############
+# add pf met
+switchToPFMET(process, input=cms.InputTag('pfMet')) #this adds uncorrected MET collection labelled "patMETs"
+
+# produce corrected MET collections (RECO)
+process.load("JetMETCorrections.Type1MET.correctionTermsPfMetType1Type2_cff")
+if "<DataType>" == "Data":
+    process.corrPfMetType1.jetCorrLabel = cms.string("ak5PFL1FastL2L3Residual")
+else:
+    process.corrPfMetType1.jetCorrLabel = cms.string("ak5PFL1FastL2L3")
+
+process.load("JetMETCorrections.Type1MET.correctionTermsPfMetType0PFCandidate_cff")
+process.load("JetMETCorrections.Type1MET.correctedMet_cff")
+
+# produce PAT MET collections
+from PhysicsTools.PatAlgos.producersLayer1.metProducer_cfi import patMETs
+process.patPfMetT0pcT1 = patMETs.clone(
+    metSource = cms.InputTag('pfMetT0pcT1'),
+    addMuonCorrections = cms.bool(False),
+    addGenMET    = cms.bool(False)
+)
+process.patPfMetT1 = patMETs.clone(
+    metSource = cms.InputTag('pfMetT1'),
+    addMuonCorrections = cms.bool(False),
+    addGenMET    = cms.bool(False)
+)
+
+# compute PUJetID and MVA MET
+process.load('JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff')
+process.pfMEtMVA.srcLeptons = cms.VInputTag("isomuons","isoelectrons","isotaus") # load default lepton selection (H2Tau) for MVA-MET
+if "<DataType>" == "Data":
+    process.calibratedAK5PFJetsForPFMEtMVA.correctors = cms.vstring("ak5PFL1FastL2L3Residual")
+else:
+    process.calibratedAK5PFJetsForPFMEtMVA.correctors = cms.string("ak5PFL1FastL2L3")
+process.PUJetMVAMetSequence = cms.Sequence( process.pfMEtMVAsequence * process.pileupJetIdProducer)
+
+process.JetMetSequence = cms.Sequence(process.correctionTermsPfMetType1Type2
+                                      * process.correctionTermsPfMetType0PFCandidate
+                                      * process.pfMetT0pcT1 
+                                      * process.pfMetT1
+                                      * process.patPfMetT0pcT1
+                                      * process.patPfMetT1
+                                      * process.PUJetMVAMetSequence)
 
 #from PhysicsTools.PatUtils.metUncertaintyTools import runMEtUncertainties
 #runMEtUncertainties(process)
 
 ####### done with setting up PAT
-
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
     #'file://dcap://grid-dcap.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/mc/Summer12/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/AODSIM/PU_S7_START52_V9-v2/0000/D2E4D132-3D97-E111-88B2-003048673FC0.root'),
-    'file://dcap://grid-dcap.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/data/Run2012A/TauPlusX/AOD/13Jul2012-v1/0000/76C17B7B-98D7-E111-B500-20CF3027A639.root'),
+    #'file://dcap://grid-dcap.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/data/Run2012A/TauPlusX/AOD/13Jul2012-v1/0000/76C17B7B-98D7-E111-B500-20CF3027A639.root'),
+                                    '/store/data/Run2012D/TauPlusX/AOD/22Jan2013-v1/20000/000260A0-4286-E211-89DC-0030487F1F23.root',
+                                    '/store/data/Run2012D/TauPlusX/AOD/22Jan2013-v1/20000/0085A19F-9187-E211-9305-0025901D484C.root',
+                                    '/store/data/Run2012D/TauPlusX/AOD/22Jan2013-v1/20000/00DF9FCD-8986-E211-B337-0025901D4936.root',
+                                    '/store/data/Run2012D/TauPlusX/AOD/22Jan2013-v1/20000/022E5139-5287-E211-9024-0030487F164D.root',
+                                    '/store/data/Run2012D/TauPlusX/AOD/22Jan2013-v1/20000/023184C4-F487-E211-AC7C-0025904B144E.root')
                             )
 
 numberOfEvents = 5000
@@ -96,20 +141,6 @@ process.CountTriggerPassedEvents.CounterType = cms.untracked.string("CountTrigge
 process.CountKinFitPassedEvents = process.EvntCounterB.clone()
 process.CountKinFitPassedEvents.CounterType = cms.untracked.string("CountKinFitPassedEvents")
                         
-####################### MET corrections ######################
-process.load("JetMETCorrections.Type1MET.pfMETCorrections_cff")
-process.pfJetMETcorr.jetCorrLabel = cms.string("ak5PFL1FastL2L3Residual")
-process.load("JetMETCorrections.Type1MET.pfMETCorrections_cff")
-process.load("JetMETCorrections.Type1MET.pfMETCorrectionType0_cfi")
-process.pfType1CorrectedMet.applyType0Corrections = cms.bool(False)
-process.pfType1CorrectedMet.srcType1Corrections = cms.VInputTag(
-    cms.InputTag('pfMETcorrType0'),
-    cms.InputTag('pfJetMETcorr', 'type1')        
-    )
-
-####################### MET corrections ######################
-process.endjob_step = cms.Path(process.endOfProcess)
-
 ###### New HPS
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
 ###### New HPS
@@ -144,7 +175,16 @@ process.NtupleMaker.doTrack= cms.untracked.bool(False)
 process.MultiTrigFilter.useTriggers = cms.vstring("HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v","HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v","HLT_IsoMu24_eta2p1_v","HLT_Mu17_Ele8_CaloIdL","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL","HLT_Mu8_Ele17_CaloIdL",
 "HLT_Mu8_Ele17_CaloIdT_CaloIsoVL","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL")
 
-process.TauNutpleSkim  = cms.Path(process.EvntCounterA*process.MultiTrigFilter*process.MuonPreselectionCuts*process.CountTriggerPassedEvents*process.patDefaultSequence*process.puJetIdSqeuence*process.PFTau*process.PreselectionCuts*process.EvntCounterB*process.NtupleMaker)
+process.TauNutpleSkim  = cms.Path(process.EvntCounterA
+                                  * process.MultiTrigFilter
+                                  * process.MuonPreselectionCuts
+                                  * process.CountTriggerPassedEvents
+                                  * process.patDefaultSequence
+                                  * process.JetMetSequence
+                                  * process.PFTau
+                                  * process.PreselectionCuts
+                                  * process.EvntCounterB
+                                  * process.NtupleMaker)
 process.schedule.append(process.TauNutpleSkim)
 
 
