41934e09d8984568cb5508cde23b3a216807e489
bkargoll <bastian.kargoll@cern.ch>
2014-03-19 11:03:01 +0100

    added MVA-MET for Mu+Tau

--- CRAB/TauNtuple_defaultConfig_cfg.py
+++ CRAB/TauNtuple_defaultConfig_cfg.py
@@ -95,13 +95,107 @@ else:
 process.load("JetMETCorrections.Type1MET.correctedMet_cff")
 
 
-# compute MVA MET
+# ------------------- compute MVA MET
 process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
-process.load('RecoMET.METPUSubtraction.mvaPFMET_leptons_cff')
+process.load('RecoMET.METPUSubtraction.mvaPFMET_leptons_cff') # definition of "default" leptons (for reference)
 process.load('RecoMET.METPUSubtraction.noPileUpPFMET_cff')
-#Specify the leptons
+# "default" MVA-MET
 process.pfMEtMVA.srcLeptons = cms.VInputTag("isomuons","isoelectrons","isotaus") # load default lepton selection (H2Tau) for MVA-MET
 process.pfMEtMVA.srcCorrJets = cms.InputTag("ak5PFJetsCorr")
+
+# MVA-MET specific for H->mu+tau channel
+# definitions taken from https://ekptrac.physik.uni-karlsruhe.de/trac/Kappa/browser/Producers/python/KMET_cff.py
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter("PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone( minNdof = cms.double(4.0), maxZ = cms.double(24.0) ),
+    src=cms.InputTag('offlinePrimaryVertices')
+)
+# Muons (definition stolen from Roger)
+process.mvaMETMuons = cms.EDFilter("MuonSelector",
+    src = cms.InputTag('muons'),
+    cut = cms.string(
+        "abs(eta)<2.1 & pt>15"                                      +
+        ## muon ID
+        "& isTrackerMuon"                                           +
+        "& isPFMuon"                                                +
+        "& globalTrack.isNonnull"                                   +
+        "& innerTrack.hitPattern.numberOfValidPixelHits    >  0"    +
+        "& innerTrack.normalizedChi2                       < 10"    +
+        "& numberOfMatches                                 >  0"    +
+        "& innerTrack.hitPattern.numberOfValidTrackerHits  >  5"    +
+        "& globalTrack.hitPattern.numberOfValidHits        >  0"    +
+        "& abs(innerTrack().dxy)                           <2.0"    +
+        ## muon isolation (w/o deltaBeta, therefore weaker selection criteria)
+        "& (pfIsolationR03.sumChargedHadronPt+pfIsolationR03.sumNeutralHadronEt+pfIsolationR03.sumPhotonEt)/pt < 0.3"
+    ),
+    filter = cms.bool(False)
+)
+# Taus for Mu+Tau channel (definition stolen from Roger)
+process.mvaMETTausMT = cms.EDFilter("PFTauSelector",
+    src = cms.InputTag('hpsPFTauProducer'),
+    BooleanOperator = cms.string("and"),
+    discriminators = cms.VPSet(              
+        cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByDecayModeFinding"                       ), selectionCut=cms.double(0.5)),
+        cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByLooseCombinedIsolationDBSumPtCorr3Hits" ), selectionCut=cms.double(0.5)),
+        cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByLooseElectronRejection"                 ), selectionCut=cms.double(0.5)),
+        cms.PSet( discriminator=cms.InputTag("hpsPFTauDiscriminationByTightMuonRejection"                     ), selectionCut=cms.double(0.5)) 
+        ),
+    cut = cms.string("abs(eta) < 2.3 && pt > 20.0 "),
+    filter = cms.bool(False)
+)
+# define MVA-Met producer (for Mu+Tau)
+from RecoJets.JetProducers.PileupJetIDParams_cfi import JetIdParams
+process.pfMetMVAMuTau = cms.EDProducer("PFMETProducerMVA",
+    srcCorrJets     = cms.InputTag('ak5PFJetsCorr'),
+    srcUncorrJets   = cms.InputTag('ak5PFJets'),
+    srcPFCandidates = cms.InputTag('particleFlow'),
+    srcVertices     = cms.InputTag('goodOfflinePrimaryVertices'),
+    srcLeptons      = cms.VInputTag("mvaMETMuons"    , "mvaMETTausMT"),
+    minNumLeptons   = cms.int32(0),
+    srcRho          = cms.InputTag('kt6PFJets','rho'),
+    globalThreshold = cms.double(-1.),#pfMet.globalThreshold,
+    minCorrJetPt    = cms.double(-1.),
+    inputFileNames  = cms.PSet(
+        U     = cms.FileInPath('RecoMET/METPUSubtraction/data/gbrmet_53_June2013_type1.root'   ),
+        DPhi  = cms.FileInPath('RecoMET/METPUSubtraction/data/gbrmetphi_53_June2013_type1.root'),
+        CovU1 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru1cov_53_Dec2012.root'        ),
+        CovU2 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru2cov_53_Dec2012.root'        ),
+    ),
+    loadMVAfromDB   = cms.bool(False),
+    is42            = cms.bool(False), # CV: set this flag to true if you are running mvaPFMET in CMSSW_4_2_x                           
+    corrector       = cms.string("ak5PFL1Fastjet"),
+    useType1        = cms.bool(True),
+    useOld42        = cms.bool(False),
+    dZcut           = cms.double(0.1),
+    impactParTkThreshold = cms.double(0.),
+    tmvaWeights     = cms.string("RecoJets/JetProducers/data/TMVAClassificationCategory_JetID_MET_53X_Dec2012.weights.xml"),
+    tmvaMethod      = cms.string("JetID"),
+    version         = cms.int32(-1),
+    cutBased        = cms.bool(False),                      
+    tmvaVariables   = cms.vstring(
+        "nvtx",
+        "jetPt",
+        "jetEta",
+        "jetPhi",
+        "dZ",
+        "beta",
+        "betaStar",
+        "nCharged",
+        "nNeutrals",
+        "dR2Mean",
+        "ptD",
+        "frac01",
+        "frac02",
+        "frac03",
+        "frac04",
+        "frac05",
+        ),
+    tmvaSpectators  = cms.vstring(),
+    JetIdParams     = JetIdParams,
+    verbosity       = cms.int32(0)
+)
+
+
 
 process.MetSequence = cms.Sequence(process.correctionTermsPfMetType1Type2
                                       * process.correctionTermsPfMetType0RecoTrack
@@ -120,7 +214,12 @@ process.MetSequence = cms.Sequence(process.correctionTermsPfMetType1Type2
                                       * process.pfMetT1Txy
                                       * process.caloMetT1
                                       * process.caloMetT1T2
-                                      * process.pfMEtMVAsequence)
+                                      * process.pfMEtMVAsequence # default MVA-MET sequence
+                                      * process.goodOfflinePrimaryVertices
+                                      * process.mvaMETMuons
+                                      * process.mvaMETTausMT
+                                      * process.pfMetMVAMuTau # customized MVA-MET
+                                      )
 
 ###############
 
